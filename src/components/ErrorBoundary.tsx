e9c8ab1517d3662dd3c8cc21a00dee568ee275d4
AkkaFranKebne <ewa.wrobel@ggmail.com>
2024-12-14 09:00:32 +0100

    lint fixes

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -21,10 +21,6 @@ class ErrorBoundary extends React.Component<
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
-    return { hasError: true };
-  }
-
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     console.error("Uncaught error:", error, errorInfo);
   }
