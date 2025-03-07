52f1cab1b0a8bd8398add86144e4eaa34f244f70
AkkaFranKebne <ewa.wrobel@gmail.com>
2025-03-07 09:35:23 +0100

    using useActionState

--- src/app/test3/PageContent.tsx
+++ src/app/test3/PageContent.tsx
@@ -1,25 +1,33 @@
 "use client";
 
+import { useActionState } from "react";
 import { fetchData } from "./actions";
-import { useState } from "react";
 import { StoryblokStory } from "@storyblok/react/rsc";
+import { Button } from "@/components/ui/button";
+
+// useActionState is a Hook that allows you to update state based on the result of a form action. It takes an action function and returns an array with the current state, the action function, and a boolean indicating if the action is pending. The action function is called with the form event as an argument. The action function should return a Promise that resolves to the new state.
 
 export default function PageContent() {
-  const [story, setStory] = useState();
+  const [state, fetchDataAction, pending] = useActionState(fetchData, null);
 
   return (
     <>
-      {!story && (
-        <button
-          onClick={async () => {
-            const { data } = await fetchData();
-            setStory(data.story);
-          }}
-        >
-          Show Page
-        </button>
+      {!state && !pending && (
+        <div className="h-screen w-full flex justify-center items-center">
+          <form action={fetchDataAction}>
+            <Button size="lg" type="submit">
+              Show Page
+            </Button>
+          </form>
+        </div>
       )}
-      {story && <StoryblokStory story={story} />}
+      {pending && (
+        <div className="h-screen w-full flex justify-center items-center">
+          <p>Loading...</p>
+        </div>
+      )}
+
+      {state && <StoryblokStory story={state.data.story} />}
     </>
   );
 }
--- src/app/test3/actions.ts
+++ src/app/test3/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { StoryblokClient, ISbStoriesParams } from "@storyblok/react";
+import { StoryblokClient, ISbStoriesParams, ISbResult } from "@storyblok/react";
 import { getStoryblokApi } from "@/lib/storyblok";
 
 // Add 'use server' at the top of an async function body to mark the function as callable by the client. We call these functions Server Functions. Server Functions are executed on the server and the result is sent to the client. This is useful for fetching data from an API, database, or other sources that are not available on the client.
